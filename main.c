5512aa056476df9d96d61afc5681ddda74ee0dff
Elkhan <74042126+ubbeg2000@users.noreply.github.com>
2023-02-08 23:11:57 +0700

    Update main.c

--- main.c
+++ main.c
@@ -11,11 +11,87 @@
 
 int main() 
 {
+    int i, n;
+
+    // input banyaknya deret
     printf("Masukkan panjang deret: ");
+    scanf("%d", &n);
 
+    //cek
+    if (n==0)
+    {
+        printf("Tidak ada deret");
+        exit(0);
+    }
+
+    int awal, akhir;
     printf("Masukkan awal interval: ");
-    printf("Masukkan akhir interval: ")
-    printf("\nBanyaknya bilangan genap pada deret Fibonacci dari A sampai B adalah C");
+    scanf("%d", &awal);
 
-    return 0;
+    //cek
+    if (awal>n)
+    {
+        printf("Tidak bisa! karena awal interval melewati panjang deret");
+        exit(0);
+    }
+
+    printf("Masukkan akhir interval: ");
+    scanf("%d", &akhir);
+
+    //cek
+    if (akhir>n)
+    {
+        printf("Tidak bisa! karena akhir interval melewati panjang deret");
+        exit(0);
+    }
+    if (awal>akhir)
+    {
+        printf("Tidak bisa! karena awal interval lebih besar dari akhir interval");
+        exit(0);
+    }
+
+    // inisialisasi baris pertama dan kedua
+    int b1 = 1, b2 = 1;
+
+    // baris selanjutnya
+    int nextTerm = b1 + b2;
+
+    // inisialisasi variabel banyaknya bilangan ganjil
+    int genap = 0;
+
+    if (awal==1)
+    {
+        if (akhir==1){
+            printf("Deret Fibonacci dari baris %d sampai %d adalah 1 ",awal,akhir);
+        }
+        else{
+            printf("Deret Fibonacci dari baris %d sampai %d adalah 1 1 ",awal,akhir);
+        }
+    }
+    else if (awal==2)
+    {
+        printf("Deret Fibonacci dari baris %d sampai %d adalah 1 ",awal,akhir);
+    }
+    else if (awal>2)
+    {
+        printf("Deret Fibonacci dari baris %d sampai %d adalah ",awal,akhir);
+    }
+
+    // output baris selanjutnya
+    for (i = 3; i <= n; ++i) {
+        if (i>=awal && i<=akhir)
+        {
+            printf("%d ", nextTerm);
+            if (nextTerm%2==0)
+            {
+                genap += 1;
+            }
+        }
+        b1 = b2;
+        b2 = nextTerm;
+        nextTerm = b1 + b2;
+    }
+    printf("\nBanyaknya bilangan genap pada deret Fibonacci dari %d sampai %d adalah %d",awal,akhir,genap);
+
+  return 0;
 }
